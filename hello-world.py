55ff96bc0dd9c07303de4f4ebac2b424b73979cb
Arjun D <arjund@Arjuns-Mac-mini.local>
2025-02-17 22:45:18 +0000

    My first program in python

---
+++ hello-world.py
@@ -0,0 +1,35 @@
+# This is a comment. Comments are ignored by Python.
+# They are used to explain what the code does.
+
+# Print a message to the screen
+print("Hello, World!")
+
+# Variables store information that can be used later
+name = "Alice"  # This is a string variable
+age = 11  # This is an integer variable
+
+# Print the values of the variables
+print("My name is", name)
+print("I am", age, "years old")
+
+# Simple arithmetic operations
+sum = 5 + 3  # Addition
+difference = 10 - 2  # Subtraction
+product = 4 * 2  # Multiplication
+quotient = 16 / 2  # Division
+
+# Print the results of the arithmetic operations
+print("5 + 3 =", sum)
+print("10 - 2 =", difference)
+print("4 * 2 =", product)
+print("16 / 2 =", quotient)
+
+# Using an if statement to make decisions
+if age > 10:
+    print("You are older than 10 years old!")
+else:
+    print("You are 10 years old or younger.")
+
+# A simple loop to repeat actions
+for i in range(5):
+    print("This is loop iteration number", i)
